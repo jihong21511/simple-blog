7a1caa61de181ca84c8a2f82286d7d2a02bdffa6
jihong21511 <jihong21511@naver.com>
2019-08-21 17:32:12 +0900

    upload a new project

---
+++ src/main/java/com/example/blog/controller/BlogController.java
@@ -0,0 +1,51 @@
+package com.example.blog.controller;
+
+
+import com.example.blog.vo.Member;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class BlogController {
+    @RequestMapping(value="/sayHello", method = RequestMethod.GET)
+    public String sayHello(){
+        return "Hello, JIhong.";
+    }
+
+    @RequestMapping(value="/getInfo", method = RequestMethod.GET)
+    public Member getInfo(){
+        Member member = new Member(1,"jihong","jihong@naver.com");
+/*        member.setId(1);
+        member.setName("jihong");
+        member.setEmail("jihong.com");*/
+        return member;
+    }
+
+    @RequestMapping(value="/list", method = RequestMethod.GET)
+    public List<Member> list(){
+        List<Member> memberList = Arrays.asList(
+                new Member[]{new Member(1,"jihong1","jihong1@naver.com"),
+                        new Member(2,"jihong1","jihong1@naver.com"),
+                        new Member(3,"jihong1","jihong1@naver.com"),
+                        new Member(4,"jihong1","jihong1@naver.com"),
+                });
+
+        long startTime = System.currentTimeMillis();
+        for (Member member : memberList) {
+            System.out.println(member);
+        }
+        System.out.println("for - loop:" + (System.currentTimeMillis() - startTime) + " ms");
+
+        startTime = System.currentTimeMillis();
+        memberList.forEach(System.out::println);
+        System.out.println("lamda:" + (System.currentTimeMillis() - startTime) + " ms");
+
+
+
+        return memberList;
+    }
+}
---
+++ src/main/java/com/example/blog/vo/Member.java
@@ -0,0 +1,30 @@
+package com.example.blog.vo;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Member {
+    private int id;
+    private String name;
+    private String email;
+
+
+    public Member(){}
+
+    public Member(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
